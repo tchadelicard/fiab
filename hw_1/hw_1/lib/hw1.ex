c8d27ef53409724c51de9c88873b25bb31ca8df2
Tchadel Icard <tchadelicard@protonmail.com>
2024-09-19 13:26:50 +0200

    init mix project for hw1

---
+++ hw_1/hw_1/lib/hw1.ex
@@ -0,0 +1,18 @@
+defmodule Hw1 do
+  @moduledoc """
+  Documentation for `Hw1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hw1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ hw_1/hw_1/test/hw1_test.exs
@@ -0,0 +1,8 @@
+defmodule Hw1Test do
+  use ExUnit.Case
+  doctest Hw1
+
+  test "greets the world" do
+    assert Hw1.hello() == :world
+  end
+end
